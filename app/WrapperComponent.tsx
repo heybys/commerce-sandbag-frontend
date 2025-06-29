3dbb4ad708214e88d83acfbfd2d64305a75d1057
heybys <byn0174@gmail.com>
2025-06-29 15:24:44 +0900

    feat: add sample image for public dir test

--- app/WrapperComponent.tsx
+++ app/WrapperComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import CountComponent from './components/CountComponent';
+import Image from "next/image";
 
 // 서버 컴포넌트는 동적 import
 const SlowServerComponent = dynamic(() => import('./SlowServerComponent'), {
@@ -12,6 +13,7 @@ const WrapperComponent = () => {
   return (
     <div>
       <h1>서버 컴포넌트 로딩 테스트</h1>
+      <Image src={'/pomeranian.jpg'} alt={'pomeranian.jpg'} width={80} height={100}/>
       <div>
         <CountComponent />
         <SlowServerComponent />
