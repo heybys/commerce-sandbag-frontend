3b2363b2ac7553d04ec54240a67981ce58120786
heybys <byn0174@gmail.com>
2025-08-10 01:00:04 +0900

    refact: reformat and remove unused lines

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import WrapperComponent from "./WrapperComponent";
-
-
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!!!</h1>
-      <div>NEXT_ENV={process.env.NEXT_ENV}</div>
-      <div>SET_ENV={process.env.SET_ENV}</div>
-      <div>NODE_ENV={process.env.NODE_ENV}</div>
-      <div>TZ={process.env.TZ}</div>
-      <WrapperComponent />
+      <div>hello</div>
     </>
   );
 }
